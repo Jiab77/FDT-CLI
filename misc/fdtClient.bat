ac847beb08fd62ce3153eff30e4d2decb450668a
Jiab77 <jonathan.barda@gmail.com>
2014-11-21 18:44:48 +0100

    Default FDT Client / Server Scripts
    Windows: *.bat
    Linux: *.sh

---
+++ misc/fdtClient.bat
@@ -0,0 +1,19 @@
+@echo off
+if "%1"=="" goto param_needed
+for %%A in (%*) do if "%%A" == "-c" goto do_exec
+echo "***************************************************"
+echo "******* Please specify the server address *********"
+echo "***************************************************"
+goto help
+:do_exec
+java -jar fdt.jar %*
+goto :EOF
+:param_needed
+        echo "***************************************************"
+        echo "************** Parameters needed ******************"
+        echo "***************************************************"
+        goto help
+:help
+        java -jar fdt.jar -h
+        goto :EOF
+:end
