e5f93158e87aa55d794aa8f31e542bb9749327df
Jiab77 <jonathan.barda@gmail.com>
2015-10-14 13:31:17 +0200

    Removed test code

--- FDT-CLI-Download.bat
+++ FDT-CLI-Download.bat
@@ -1,9 +1,8 @@
 @echo off
 for %%a in (cls echo echo) do %%a.
-REM set host=37.252.129.13
-set /p host=Server : 
-set /p src=Source : 
-set /p dst=Destination : 
+set /p host=Server :
+set /p src=Source :
+set /p dst=Destination :
 if /i not "%dst%"=="" (
 	if not exist "%dst%" mkdir "%dst%"
 )
